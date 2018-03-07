9aac93d89ba45ead1b883721c006b128c5b5b60a
Chely Ho <adept_inept@yahoo.com>
2018-03-07 16:26:47 +0000

    updated required filepaths

--- lib/top_100_films.rb
+++ lib/top_100_films.rb
@@ -4,5 +4,5 @@ require 'colorize'
 
 require_relative "./top_100_films/version"
 require_relative "./top_100_films/cli"
-require_relative "./top_100_films/films"
+require_relative "./top_100_films/film"
 require_relative "./top_100_films/scraper"
